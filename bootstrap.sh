7bfb4dee93f0ff97dbeea28350a3055692382b2d
Felipe Leusin <felipe.leusin@shopify.com>
2021-07-07 09:55:59 -0400

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+cp ~/dotfiles/.gitconfig ~/
+cp ~/dotfiles/spin.zshrc ~/
+
+# Only run nested steps in Spin workspaces.
+if [[ "$SPIN" = 1 ]]
+then
+  # Only run nested steps in Spin + shopify/shopify workspaces.
+  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
+  then
+    cartridge insert felipeleusin/returns-team-bootstrap
+    . /cartridges/email-team-bootstrap/setup.sh
+  fi
+fi
