5d11e5b46ad0ffd131164565be9e8cbc94e7e42d
Felipe Leusin <felipe.leusin@shopify.com>
2023-11-24 10:10:51 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,3 @@
 
 ln -sf ~/dotfiles/spin/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/spin/.zshrc ~/.zshrc
-
-# Only run nested steps in Spin workspaces.
-if [[ "$SPIN" = 1 ]]
-then
-  # Only run nested steps in Spin + shopify/shopify workspaces.
-  if [[ "$SPIN_REPO_SOURCE_PATH" = "/home/src/github.com/shopify/shopify" ]]
-  then
-    cartridge insert felipeleusin/email-team-bootstrap
-    . ~/.data/cartridges/email-team-bootstrap/setup.sh
-  fi
-fi
